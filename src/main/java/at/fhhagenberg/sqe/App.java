3555d4169a3a0b3e74d7bdf97340165f5a5e24d6
Stephan Schuler <S2010567024@fhooe.at>
2022-01-18 19:25:27 +0100

    Update App.java

--- src/main/java/at/fhhagenberg/sqe/App.java
+++ src/main/java/at/fhhagenberg/sqe/App.java
@@ -24,89 +24,89 @@ import java.rmi.NotBoundException;
  * JavaFX App
  */
 
-public class App extends Application 
-{
+public class App extends Application {
     private eccGUI gui;
     protected ElevatorControlCenter elContr;
-    private ElevatorWrapper eleWrap;
+    private Boolean threadTerminate = false;
+    private IElevator elevator;
 
-    private static IElevator elevator;
+    protected ElevatorWrapper createWrapper() {
+        try {
+            elevator = (IElevator) Naming.lookup("rmi://127.0.0.1/ElevatorSim");
 
-    
-    protected ElevatorWrapper createWrapper()
-    {
-    	try {
-			elevator = (IElevator) Naming.lookup("rmi://127.0.0.1/ElevatorSim");
-	        
-		} catch (MalformedURLException | RemoteException | NotBoundException e) {
-			e.printStackTrace();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-    	return new ElevatorWrapper(elevator);
+        } catch (MalformedURLException | RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+        return new ElevatorWrapper(elevator);
     }
-    
-    protected eccGUI createGUI()
-    {
+
+    protected eccGUI createGUI() {
         elContr = new ElevatorControlCenter(createWrapper());
-		try {
-        elContr.update();
-		} catch(RuntimeException e){ e.printStackTrace(); }
+        try {
+            elContr.update();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
 
-    	return new eccGUI(elContr, 1280, 960);
-    	
+        return new eccGUI(elContr, 1280, 960);
+
     }
-    
-    @Override
-    public void start(Stage stage)
-    {	
-		try{	this.gui = createGUI();
-	           }catch (RuntimeException ex) { ex.printStackTrace(); }
-		gui.init();
 
-    	// background task
+    @Override
+    public void start(Stage stage) {
+        try {
+            this.gui = createGUI();
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+        }
+        gui.init();
+
+        // background task
         Task<Void> task = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
-            	boolean connected = true;
-                while (true) {
+                boolean connected = true;
+                while (!threadTerminate) {
                     Thread.sleep(30);
-                	try {
-                		if(!connected) {
-                			Thread.sleep(3000);
-                			connected = true;
-							elContr.setServer(createWrapper());
-							elContr.InitElevatorAndFloors();
-                			Platform.runLater(() -> {
-			                				gui.init();
-			                				gui.setConnState(true);
-			                		    	gui.start(stage);
-			                });
+                    try {
+                        if (!connected) {
+                            Thread.sleep(3000);
+                            connected = true;
+                            elContr.setServer(createWrapper());
+                            elContr.InitElevatorAndFloors();
+                            Platform.runLater(() -> {
+                                gui.init();
+                                gui.setConnState(true);
+                                gui.start(stage);
+                            });
 
-                		} else {
-                			elContr.update();
-                			Platform.runLater(() -> { gui.update(); });
-                		}
-                		
-            		} catch (RuntimeException e) {
-            			// TODO Auto-generated catch block
-            			e.printStackTrace();
-            			Platform.runLater(() -> {
-            				gui.setConnState(false);
-                        });
-            			connected = false;
-            		} catch (Exception e) {
-            			System.out.print(e.getMessage());
-            		}
+                        } else {
+                            elContr.update();
+                            Platform.runLater(() -> gui.update());
+                        }
+
+                    } catch (RuntimeException e) {
+                        e.printStackTrace();
+                        Platform.runLater(() -> gui.setConnState(false));
+                        connected = false;
+                    } catch (Exception e) {
+                        System.out.print(e.getMessage());
+                        Thread.currentThread().interrupt();
+                    }
                 }
+                return null;
             }
         };
 
-		Thread thd = new Thread(task);
-		thd.setDaemon(true);
-		thd.start();
+        Thread thd = new Thread(task);
+        thd.start();
 
-		gui.start(stage);
+        gui.start(stage);
+    }
+
+    @Override
+    public void stop() {
+        threadTerminate = true;
     }
 
     public static void main(String[] args) {
